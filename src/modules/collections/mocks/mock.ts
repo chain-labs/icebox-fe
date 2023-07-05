572cc385523ccd10d54311f35ad69ada6072b2d4
Angel Lakra <angel.lakra@chainlabs.in>
2023-07-05 22:25:21 +0530

    added mock data from bestinslot website api

---
+++ src/modules/collections/mocks/mock.ts
@@ -0,0 +1,244 @@
+import { TableItemProps } from '../components/TableItem'
+
+export const ITEMS: TableItemProps[] = [
+    {
+        slug: 'smiley-cones',
+        name: 'Smiley Cones',
+        inscriptions: {
+            max: 383467,
+            min: 329826,
+        },
+        image_url:
+            'https://bis-ord-content.fra1.cdn.digitaloceanspaces.com/ordinals/67f1a7ccb6491ae3f9d799501f0a8e41c51ff36b4bf3a83938a06237f3bc6bb1i0',
+        floor_price: 99984,
+        top_loan_offer: null,
+        loan_volume_7d: {
+            value: 25155.25,
+            change: 24.63,
+        },
+        sales_volume_7d: {
+            value: 12566.05,
+            change: -16.9,
+        },
+        buy_links: {
+            ship: true,
+            gem: true,
+        },
+    },
+    {
+        slug: 'spiritkits',
+        name: 'SPIRITKITS',
+        inscriptions: {
+            max: 11437555,
+            min: 11434243,
+        },
+        image_url:
+            'https://bis-ord-content.fra1.cdn.digitaloceanspaces.com/ordinals/3bb05ecfa5c65843d577531086772e5cb6945948287e5368e83f799865c3be41i0',
+        floor_price: 250000,
+        top_loan_offer: 2000,
+        loan_volume_7d: {
+            value: 5155.25,
+            change: -2.05,
+        },
+        sales_volume_7d: {
+            value: null,
+            change: null,
+        },
+        buy_links: {
+            ship: true,
+            gem: false,
+        },
+    },
+    {
+        slug: 'slice-of-life-pepe',
+        name: 'Slice of Life Pepe',
+        inscriptions: {
+            max: 138560,
+            min: 138600,
+        },
+        image_url:
+            'https://bis-ord-content.fra1.cdn.digitaloceanspaces.com/ordinals/bf705c29aa6cbf3d1378f4257affa1a929a05b4a3c6d7d7e3b2bbb010881154ei0',
+        floor_price: 600012,
+        top_loan_offer: 600000,
+        loan_volume_7d: {
+            value: null,
+            change: null,
+        },
+        sales_volume_7d: {
+            value: 32556.55,
+            change: 19.9,
+        },
+        buy_links: {
+            ship: false,
+            gem: false,
+        },
+    },
+    {
+        slug: 'uhho-ordinals',
+        name: 'uhho Ordinals',
+        inscriptions: {
+            min: 9830970,
+            max: 11628211,
+        },
+        image_url:
+            'https://bis-ord-content.fra1.cdn.digitaloceanspaces.com/ordinals/1179a912da2340ab37efe15a568dcf9598252a79572a0f9d442e942283baa0d9i0',
+        floor_price: 150012,
+        top_loan_offer: 170002,
+        loan_volume_7d: {
+            value: 25155.25,
+            change: 24.63,
+        },
+        sales_volume_7d: {
+            value: 12566.05,
+            change: -16.9,
+        },
+        buy_links: {
+            ship: false,
+            gem: true,
+        },
+    },
+    {
+        slug: 'diypunks',
+        name: 'D.I.Y. Punks',
+        inscriptions: {
+            min: 9959200,
+            max: 10286852,
+        },
+        image_url:
+            'https://bis-ord-content.fra1.cdn.digitaloceanspaces.com/ordinals/9b5d37f68447d26a22472883f62413e60035ea852a7ceae2d9c1cf5c43297a99i0',
+        floor_price: 99984,
+        top_loan_offer: null,
+        loan_volume_7d: {
+            value: 25155.25,
+            change: 24.63,
+        },
+        sales_volume_7d: {
+            value: 12566.05,
+            change: -16.9,
+        },
+        buy_links: {
+            ship: true,
+            gem: true,
+        },
+    },
+    {
+        slug: 'smiley-cones',
+        name: 'Smiley Cones',
+        inscriptions: {
+            max: 383467,
+            min: 329826,
+        },
+        image_url:
+            'https://bis-ord-content.fra1.cdn.digitaloceanspaces.com/ordinals/67f1a7ccb6491ae3f9d799501f0a8e41c51ff36b4bf3a83938a06237f3bc6bb1i0',
+        floor_price: 99984,
+        top_loan_offer: null,
+        loan_volume_7d: {
+            value: 25155.25,
+            change: 24.63,
+        },
+        sales_volume_7d: {
+            value: 12566.05,
+            change: -16.9,
+        },
+        buy_links: {
+            ship: true,
+            gem: true,
+        },
+    },
+    {
+        slug: 'smiley-cones',
+        name: 'Smiley Cones',
+        inscriptions: {
+            max: 383467,
+            min: 329826,
+        },
+        image_url:
+            'https://bis-ord-content.fra1.cdn.digitaloceanspaces.com/ordinals/67f1a7ccb6491ae3f9d799501f0a8e41c51ff36b4bf3a83938a06237f3bc6bb1i0',
+        floor_price: 99984,
+        top_loan_offer: null,
+        loan_volume_7d: {
+            value: 25155.25,
+            change: 24.63,
+        },
+        sales_volume_7d: {
+            value: 12566.05,
+            change: -16.9,
+        },
+        buy_links: {
+            ship: true,
+            gem: true,
+        },
+    },
+    {
+        slug: 'smiley-cones',
+        name: 'Smiley Cones',
+        inscriptions: {
+            max: 383467,
+            min: 329826,
+        },
+        image_url:
+            'https://bis-ord-content.fra1.cdn.digitaloceanspaces.com/ordinals/67f1a7ccb6491ae3f9d799501f0a8e41c51ff36b4bf3a83938a06237f3bc6bb1i0',
+        floor_price: 99984,
+        top_loan_offer: null,
+        loan_volume_7d: {
+            value: 25155.25,
+            change: 24.63,
+        },
+        sales_volume_7d: {
+            value: 12566.05,
+            change: -16.9,
+        },
+        buy_links: {
+            ship: true,
+            gem: true,
+        },
+    },
+    {
+        slug: 'smiley-cones',
+        name: 'Smiley Cones',
+        inscriptions: {
+            max: 383467,
+            min: 329826,
+        },
+        image_url:
+            'https://bis-ord-content.fra1.cdn.digitaloceanspaces.com/ordinals/67f1a7ccb6491ae3f9d799501f0a8e41c51ff36b4bf3a83938a06237f3bc6bb1i0',
+        floor_price: 99984,
+        top_loan_offer: null,
+        loan_volume_7d: {
+            value: 25155.25,
+            change: 24.63,
+        },
+        sales_volume_7d: {
+            value: 12566.05,
+            change: -16.9,
+        },
+        buy_links: {
+            ship: true,
+            gem: true,
+        },
+    },
+    {
+        slug: 'smiley-cones',
+        name: 'Smiley Cones',
+        inscriptions: {
+            max: 383467,
+            min: 329826,
+        },
+        image_url:
+            'https://bis-ord-content.fra1.cdn.digitaloceanspaces.com/ordinals/67f1a7ccb6491ae3f9d799501f0a8e41c51ff36b4bf3a83938a06237f3bc6bb1i0',
+        floor_price: 99984,
+        top_loan_offer: null,
+        loan_volume_7d: {
+            value: 25155.25,
+            change: 24.63,
+        },
+        sales_volume_7d: {
+            value: 12566.05,
+            change: -16.9,
+        },
+        buy_links: {
+            ship: true,
+            gem: true,
+        },
+    },
+]
