624bd26fba6cfad6832285ea50fefa29fda3bc29
Angel Lakra <angel.lakra@chainlabs.in>
2023-07-13 02:31:15 +0530

    added collection api and added api to table

--- src/modules/collections/Table/index.tsx
+++ src/modules/collections/Table/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TableHeader from '../components/TableHeader'
-import TableItem from '../components/TableItem'
-import { ITEMS } from '../mocks/mock'
+import TableItem, { TableItemProps } from '../components/TableItem'
+import axios from 'axios'
+import _ from 'lodash'
 
 const Table = () => {
+    const [items, setItems] = useState<TableItemProps[]>([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/collections').then((res) => {
+            setItems(_.slice(res.data, 0, 11))
+        })
+    }, [])
+
     return (
         <div className="w-max-[1320px]">
             <TableHeader />
-            {ITEMS.map((item) => (
+            {items.map((item) => (
                 <TableItem
                     key={item.slug}
+                    slug={item.slug}
                     name={item.name}
                     inscriptions={item.inscriptions}
                     image_url={item.image_url}
---
+++ src/pages/collections.tsx
@@ -0,0 +1,15 @@
+import Table from '@/modules/collections/Table'
+import React from 'react'
+
+const CollectionsPage = () => {
+    return (
+        <div>
+            <div>Collections Table</div>
+            <div>
+                <Table />
+            </div>
+        </div>
+    )
+}
+
+export default CollectionsPage
