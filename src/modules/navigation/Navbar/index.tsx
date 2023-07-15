cbe2ce7f77b7bc1d7ade33a31aeccc28433a6211
Angel Lakra <angel.lakra@chainlabs.in>
2023-07-15 17:15:45 +0530

    added home link to logo

--- src/modules/navigation/Navbar/index.tsx
+++ src/modules/navigation/Navbar/index.tsx
@@ -3,16 +3,23 @@ import React from 'react'
 import SearchBar from '../Searchbar'
 import NavLink from './components/NavLink'
 import { BRANDING } from '@/common/constants'
+import Link from 'next/link'
 
 const Navbar = ({ page }) => {
     return (
         <div className="w-full h-14 px-[60px] py-2 bg-white bg-opacity-80 border-b border-b-slate-700 border-opacity-30 flex justify-between">
             <div className="flex items-center">
                 <div className="text-black flex">
-                    <div className="h-10 w-[108px] relative cursor-pointer">
-                        <Image fill src={BRANDING} alt="logo-branding" />
-                    </div>
-                    <NavLink text="Collections" page={page} url="/collections" />
+                    <Link href="/">
+                        <div className="h-10 w-[108px] relative cursor-pointer">
+                            <Image fill src={BRANDING} alt="logo-branding" />
+                        </div>
+                    </Link>
+                    <NavLink
+                        text="Collections"
+                        page={page}
+                        url="/collections"
+                    />
                     <NavLink text="Portfolio" page={page} url="/portfolio" />
                 </div>
             </div>
